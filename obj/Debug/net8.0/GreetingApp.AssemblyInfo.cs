dbc3385c4e94bdd60f0afe6c37db22be2bb146a0
xianyuchen2077 <zhengyan24@mails.tsinghua.edu.cn>
2025-08-13 21:56:55 +0800

    initial commit

--- obj/Debug/net8.0/GreetingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/GreetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d537ac21ce1083aee80472ac87c2fd446f3d78f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb72e8e4928562fda53ce8a296c07f615036c9af")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
